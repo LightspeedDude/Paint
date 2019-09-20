24d68fb4373e5f2b70bb22bd3eceb12bd4f7efdb
LightspeedDude <julio_martinez25@live.com>
2019-09-20 00:23:44 -0400

    Add project files.

---
+++ Paint/Paint/MainPage.xaml.cs
@@ -0,0 +1,74 @@
+﻿using SkiaSharp;
+using SkiaSharp.Views.Forms;
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Paint
+{
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        private List<SKPath> paths = new List<SKPath>();
+        private Dictionary<long, SKPath> temporaryPaths = new Dictionary<long, SKPath>();
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnPainting(object sender, SKPaintSurfaceEventArgs e)
+        {
+            var surface = e.Surface;
+
+            var canvas = surface.Canvas;
+
+            canvas.Clear(SKColors.White);
+
+            var stroke = new SKPaint
+            {
+                Color = SKColors.DarkOrange,
+                StrokeWidth = 2,
+                Style = SKPaintStyle.Stroke
+            };
+
+
+            foreach (var touchPath in paths)
+            {
+                canvas.DrawPath(touchPath, stroke);
+            }
+
+
+        }
+
+        private void OnTouch(object sender, SKTouchEventArgs e)
+        {
+            switch (e.ActionType)
+            {
+                case SKTouchAction.Pressed:
+                    var p = new SKPath();
+                    p.MoveTo(e.Location);
+                    temporaryPaths[e.Id] = p;
+                    break;
+                case SKTouchAction.Moved:
+                    if (e.InContact)
+                        temporaryPaths[e.Id].LineTo(e.Location);
+                    break;
+                case SKTouchAction.Released:
+                    paths.Add(temporaryPaths[e.Id]);
+                    temporaryPaths.Remove(e.Id);
+                    break;
+            }
+
+            e.Handled = true;
+
+            // update the UI on the screen
+            ((SKCanvasView)sender).InvalidateSurface();
+        }
+
+    }
+}
