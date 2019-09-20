e0d9ebd0c8e4017b7980e9a3c0582ec11af4d7f4
LightspeedDude <julio_martinez25@live.com>
2019-09-20 11:40:11 -0400

    missing save

--- Paint/Paint/MainPage.xaml.cs
+++ Paint/Paint/MainPage.xaml.cs
@@ -16,6 +16,7 @@ namespace Paint
     {
         private List<SKPath> paths = new List<SKPath>();
         private Dictionary<long, SKPath> temporaryPaths = new Dictionary<long, SKPath>();
+        private Color but;
         public MainPage()
         {
             InitializeComponent();
@@ -23,19 +24,25 @@ namespace Paint
 
         private void OnPainting(object sender, SKPaintSurfaceEventArgs e)
         {
-            var surface = e.Surface;
+            SKSurface surface = e.Surface;
 
-            var canvas = surface.Canvas;
+            SKCanvas canvas = surface.Canvas;
 
             canvas.Clear(SKColors.White);
 
-            var stroke = new SKPaint
+            if (but == null)
             {
-                Color = SKColors.DarkOrange,
-                StrokeWidth = 2,
-                Style = SKPaintStyle.Stroke
-            };
+                but = Color.Red;
+            }
 
+            SKPaint stroke = new SKPaint
+            {
+
+                Color = but.ToSKColor(),
+                StrokeWidth = 2,
+                Style = SKPaintStyle.Stroke,
+                StrokeCap = SKStrokeCap.Round
+            };
 
             foreach (var touchPath in paths)
             {
@@ -66,9 +73,45 @@ namespace Paint
 
             e.Handled = true;
 
-            // update the UI on the screen
             ((SKCanvasView)sender).InvalidateSurface();
         }
+
+        void OnClear(object sender, SKPaintSurfaceEventArgs e)
+        {
+            var surface = e.Surface;
+
+            var canvas = surface.Canvas;
+
+            canvas.Clear(SKColors.White);
+        }
+
+        public void OnChange(Object ob, EventArgs ar)
+        {
+            var button = (Button)ob;
+            but = button.BackgroundColor;
+        }
+
+        //async void OnSaving(object sender, EventArgs e) {
+
+            //using (SKImage image = SKImage.FromBitmap(saveBitmap))
+            //{
+            //    SKData data = image.Encode();
+            //    DateTime dt = DateTime.Now;
+            //    string filename = String.Format("FingerPaint-{0:D4}{1:D2}{2:D2}-{3:D2}{4:D2}{5:D2}{6:D3}.png",
+            //                                    dt.Year, dt.Month, dt.Day, dt.Hour, dt.Minute, dt.Second, dt.Millisecond);
+
+            //    IPhotoLibrary photoLibrary = DependencyService.Get<IPhotoLibrary>();
+            //    bool result = await photoLibrary.SavePhotoAsync(data.ToArray(), "FingerPaint", filename);
+
+            //    if (!result)
+            //    {
+            //        await DisplayAlert("FingerPaint", "Artwork could not be saved. Sorry!", "OK");
+            //    }
+            //}
+
+
+
+        //}
 
     }
 }
